c04919a4ecf3d8056578a44ba4a0ad2a2fd57368
Bharanidharan2K <bharanikkab123@gmail.com>
2021-09-06 10:57:02 +0530

    cors implement

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const userRoute = require('./api/users/user.router');
 
 app.use(express.json());
+app.use(cors({ origin : "*"}));
 app.use('/api/users', userRoute);
 
 app.listen(process.env.APP_PORT, () =>{
